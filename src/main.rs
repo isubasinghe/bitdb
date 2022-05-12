8450efcead8a452f088c14f49bb1d5295ae9a10b
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-05-13 00:07:51 +0200

    added shitty benchmark

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use bitcask::memstore::MemStore;
 use bitcask::dmap::DMap;
 use bitcask::dirman::Dirman;
 use bitcask::errors::BitcaskError;
-
+use std::time::Instant;
 fn main() -> Result<(), BitcaskError> {
     let mut dman = Dirman::<DMap>::open("/tmp/".to_string())?;
-    dman.put("Hello World".to_string(), "Bye Bye World\n".to_string())?;
+    let start = Instant::now(); 
 
-    let x: String = dman.get_as("Hello World".to_string())?;
+    for i in 0..100000 {
+        dman.put(format!("KEY:{}", i), "Bye Bye World\n".to_string())?;
+    }
+    let end = Instant::now();
 
-    print!("RETRIEVED {}", x);
+    let diff = end-start;
+    println!("100000 inserts completed in {}ms", diff.as_millis());
+    println!("{}ns per insert", diff.as_nanos()/100000);
     
     Ok(())
 }
